87841bc9e113f218c37e45e1e966f2fd9611fd8f
Rohan Mishra <rohmish26@gmail.com>
2018-04-24 13:54:52 +0530

    modified js

--- static/js/generator.js
+++ static/js/generator.js
@@ -19,7 +19,7 @@ let serviceURL = baseURL + 'api/get_code';
 console.log( document.cookie.split );
 let appid = '5aded9ec734d1d0b3c2f3cec'; // Get dynamically from post request to page in future.
 let sessionid = 0001; // Get dynamically from post request to page in future.
-appkey = document.cookie.valueOf( 'key' ) || '579823yr3uwey';
+let appkey = document.cookie.valueOf( 'key' ) || '579823yr3uwey';
 
 let data = {
   appid: appid,
